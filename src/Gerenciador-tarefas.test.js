52bb901d2cd9e1fb4b5e5ff12c117dc947a9f904
fxavierds <fxavierds@yahoo.com.br>
2020-08-26 22:48:09 -0300

    continuando

--- src/Gerenciador-tarefas.test.js
+++ src/Gerenciador-tarefas.test.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GerenciadorTarefas from './Gerenciador-tarefas';
+
+it('deve renderizar o projeto sem erros', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<GerenciadorTarefas />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
--- src/Listar/Listar-tarefas.js
+++ src/Listar/Listar-tarefas.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import {A} from 'hookrouter';
+import { Table, Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function ListarTarefas(){
-    return <h1>Listar tarefas</h1>
+    return (
+        <div className="text-center">
+            <h3>Tarefas a fazer</h3>
+            <Table striped bordered hover responsive data-testid="tabela">
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>                   
+                        <th>
+                            <A href="/cadastrar"
+                            className="btn btn-success btn-sm"
+                            data-testid="btn-nova-tarefa">
+                        <   FontAwesomeIcon icon={faPlus} />
+                                &nbsp;
+                                Nova tarefa
+                            </A>
+                        </th>
+                    </tr>
+                </thead>
+            </Table>
+        </div>
+    );
 }
 
 export default ListarTarefas;
